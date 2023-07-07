f26dc209c7e523706872b1b1c8dae64ad3a1bba3
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-07-08 02:02:49 +0530

    Update settings.py

--- cysec/settings.py
+++ cysec/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+SESSION_COOKIE_AGE = 60 * 60 * 1
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
