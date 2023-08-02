6d975477c6c497a83949fd4c6aa48dd59fa155b8
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-08-03 00:11:54 +0530

    media solved

--- cysec/settings.py
+++ cysec/settings.py
@@ -130,11 +130,15 @@ USE_TZ = False
 
 
 
-STATIC_URL = '/static/'
-# STATIC_URL = os.path.join(BASE_DIR, 'static/')
+# STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
  
+ 
+# MEDIA_URL = "media/"
+MEDIA_URL = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
 
--- cysec/urls.py
+++ cysec/urls.py
@@ -32,5 +32,14 @@ urlpatterns = [
 
 from django.conf import settings
 from django.conf.urls.static import static
-# if settings.DEBUG:
-#     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    settings.DEBUG = True
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    settings.DEBUG = False
