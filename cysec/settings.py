7bac27170a2d3273594d0d99bdc7ee246c8fa211
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-08-02 22:57:42 +0530

    testing media

--- cysec/settings.py
+++ cysec/settings.py
@@ -130,13 +130,13 @@ USE_TZ = False
 
 
 
-#STATIC_URL = 'static/'
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = 'static/'
+STATIC_URL = '/static/'
+# STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL =  'media/'
+ 
+MEDIA_ROOT = 'media/'
+MEDIA_URL =  os.path.join(BASE_DIR, 'media/')
 
 
 
