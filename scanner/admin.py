a1a1882c6ba68785c1f5e8c020dd6b77bc12ee88
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-09-20 00:43:47 +0530

    Update admin.py

--- scanner/admin.py
+++ scanner/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_id', 'event_name', 'event_date')
+    search_fields = ('event_name', )
+    list_filter = ('event_date', )
+
+@admin.register(scanning)
+class ScanningAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    search_fields = ('email',)
