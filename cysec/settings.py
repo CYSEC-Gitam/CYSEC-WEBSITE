22d8ae444f998e699cc1af48786587bd66ee1024
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-09-27 23:10:27 +0530

    Update settings.py

--- cysec/settings.py
+++ cysec/settings.py
@@ -132,8 +132,8 @@ USE_TZ = False
 
 
 
-STATIC_URL = '/static/'
-#STATIC_URL = os.path.join(BASE_DIR, 'static/')
+# STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
  
